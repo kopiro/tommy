86ba8aee69486a9034b3fd30ca7ff8e506c7e17a
Flavio De Stefano <destefano.flavio@gmail.com>
2018-10-09 12:17:36 +0200

    Adding publish

---
+++ publish.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DOCKER_IMAGE="kopiro/tommy"
+NPM_VERSION=$(node -e 'process.stdout.write(require("./app/package.json").version)')
+
+npm publish
+
+docker build -t "$DOCKER_IMAGE:$NPM_VERSION" . && \
+docker push "$DOCKER_IMAGE:$NPM_VERSION"
