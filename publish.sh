0f8cc71320baac7dce6f1dbdec6083e44a46bd91
Flavio De Stefano <destefano.flavio@gmail.com>
2018-10-09 12:23:04 +0200

    Fixing publish script

--- publish.sh
+++ publish.sh
@@ -3,8 +3,8 @@
 DOCKER_IMAGE="kopiro/tommy"
 NPM_VERSION=$(node -e 'process.stdout.write(require("./app/package.json").version)')
 
-pushd ./app && \
-npm publish && \
+pushd ./app
+npm publish
 popd
 
 docker build -t "$DOCKER_IMAGE:$NPM_VERSION" . && \
