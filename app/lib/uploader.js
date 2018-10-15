810e8ef143188312881996033fcb3844397e4913
Flavio De Stefano <destefano.flavio@gmail.com>
2018-10-15 10:23:20 +0200

    Refactor modu

--- app/lib/uploader.js
+++ app/lib/uploader.js
@@ -1,13 +1,14 @@
 const util = require('./util');
+const Tommy = require('..');
 
 async function syncFromRemote(bucket) {
-	return util.execPromise(`aws s3 sync "s3://${bucket}" "${global.__dst}" --exclude ".DS_Store"`, {
+	return util.execPromise(`aws s3 sync "s3://${bucket}" "${Tommy.__dst}" --exclude ".DS_Store"`, {
 		verbose: true
 	});
 }
 
 async function syncToRemote(bucket) {
-	return util.execPromise(`aws s3 sync "${global.__dst}" "s3://${bucket}" --exclude ".DS_Store"`, {
+	return util.execPromise(`aws s3 sync "${Tommy.__dst}" "s3://${bucket}" --exclude ".DS_Store"`, {
 		verbose: true
 	});
 }
