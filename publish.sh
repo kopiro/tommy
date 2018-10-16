8a4225ca2983f09bb6481006c4b8b34ac17b91cd
Flavio De Stefano <destefano.flavio@gmail.com>
2018-10-16 17:33:28 +0200

    Fix script

--- publish.sh
+++ publish.sh
@@ -6,7 +6,7 @@ NPM_VERSION=$(node -e 'process.stdout.write(require("./package.json").version)')
 npm publish
 
 docker build -t "$DOCKER_IMAGE:$NPM_VERSION" . && \
-docker push "$DOCKER_IMAGE:$NPM_VERSION" &&
+docker push "$DOCKER_IMAGE:$NPM_VERSION" && \
 
 docker tag "$DOCKER_IMAGE:$NPM_VERSION" "$DOCKER_IMAGE:latest" && \
 docker push "$DOCKER_IMAGE:latest"
