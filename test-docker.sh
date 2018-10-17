a960e05a1c55ca02f587de116b413e86c5e02895
Flavio De Stefano <destefano.flavio@gmail.com>
2018-10-17 16:37:00 +0200

    Compile FONTFORGE into container - installation via APT is corrupted

--- test-docker.sh
+++ test-docker.sh
@@ -8,12 +8,13 @@ touch .config.ext.json
 docker build -t $DOCKER_IMAGE . && \
 
 docker run -t \
--v "$(pwd)/test/src":"/src" \
--v "$(pwd)/test/dst":"/dst" \
+-v "$(pwd)/.test/src":"/src" \
+-v "$(pwd)/.test/dst":"/dst" \
 -v "$(pwd)/.config.ext.json":"/root/config.ext.json" \
 -v "$(pwd)/.awscredentials":"/root/.aws/credentials" \
 kopiro/tommy \
 --src "/src" \
 --dst "/dst" \
 --config "/root/config.ext.json" \
+-e "DEBUG=1" \
 $1
