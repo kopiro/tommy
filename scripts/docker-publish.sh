05f5e09354fae25748a1e43ec0d18e9a27ab43c5
Flavio De Stefano <destefano.flavio@gmail.com>
2019-05-23 20:30:23 +0200

    Docker publish should pull first

--- scripts/docker-publish.sh
+++ scripts/docker-publish.sh
@@ -3,7 +3,8 @@
 DOCKER_IMAGE="kopiro/tommy"
 NPM_VERSION=$(node -e 'process.stdout.write(require("./package.json").version)')
 
-docker build -t "$DOCKER_IMAGE:$NPM_VERSION" . && \
+docker pull "$DOCKER_IMAGE" &&
+docker build --cache-from "$DOCKER_IMAGE" -t "$DOCKER_IMAGE:$NPM_VERSION" . && \
 docker push "$DOCKER_IMAGE:$NPM_VERSION" && \
 
 docker tag "$DOCKER_IMAGE:$NPM_VERSION" "$DOCKER_IMAGE:latest" && \
