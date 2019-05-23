e6b705d9be804fa40715258f115b03c6456fd6a9
Flavio De Stefano <flaviod@spotify.com>
2019-05-23 11:43:59 +0200

    Using different docker-publish cmd

--- scripts/docker-publish.sh
+++ scripts/docker-publish.sh
@@ -3,8 +3,6 @@
 DOCKER_IMAGE="kopiro/tommy"
 NPM_VERSION=$(node -e 'process.stdout.write(require("./package.json").version)')
 
-npm publish
-
 docker build -t "$DOCKER_IMAGE:$NPM_VERSION" . && \
 docker push "$DOCKER_IMAGE:$NPM_VERSION" && \
 
