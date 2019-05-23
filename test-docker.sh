5f32215f9e89435fc816f78646b74fea94383abd
Flavio De Stefano <destefano.flavio@gmail.com>
2019-05-23 19:14:39 +0200

    Ensure that image is built from cache

--- test-docker.sh
+++ test-docker.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 DOCKER_IMAGE="kopiro/tommy"
+CONTAINER_NAME="kopiro-tommy"
 
 mkdir -p .test/src .test/dst
-touch .config.ext.json
+if [ ! -f .config.ext.json ]; then
+    echo "{}" > .config.ext.json
+fi
 
-docker kill tommy
-docker rm tommy
-
-docker build -t $DOCKER_IMAGE . && \
+docker kill "$CONTAINER_NAME" 
+docker rm "$CONTAINER_NAME"
+docker pull "$DOCKER_IMAGE"
+docker build -t "$DOCKER_IMAGE" --cache-from "$DOCKER_IMAGE" .
 
 docker run -t \
 -v "$(pwd)/.test/src":"/src" \
 -v "$(pwd)/.test/dst":"/dst" \
 -v "$(pwd)/.config.ext.json":"/root/config.ext.json" \
 -e "DEBUG=1" \
---name tommy \
+--name "$CONTAINER_NAME" \
 kopiro/tommy \
 --src "/src" \
 --dst "/dst" \
